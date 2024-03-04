9240f61dc27ab459d435b13d588e0ae9a2732f0a
Giacomo Santilli <giaky2005santy@gmail.com>
2024-03-04 12:01:08 +0100

    Added migrations for creating a game table and a savefile table

---
+++ database/migrations/2024_03_04_095551_create_savefile_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    protected $table = 'savefile';
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('savefile', function (Blueprint $table) {
+            $table->id();
+            $table->string('file_name');
+            $table->timestamps();
+            $table->foreignId('fk_id_game')->constrained('game');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('savefile');
+    }
+};
