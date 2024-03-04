05062baf7aeaf8a32b176763306bd3c4d9456b86
Giacomo Santilli <giaky2005santy@gmail.com>
2024-03-04 15:04:23 +0100

    Add Game and Savefile models

---
+++ app/Models/Savefile.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Savefile extends Model
+{
+    use HasFactory;
+
+    protected $table = 'savefile';
+}
