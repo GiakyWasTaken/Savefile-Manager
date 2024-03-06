81ccd5b74a89fb4bbd56f8ecdf643f59c8b8b903
Giacomo Santilli <giaky2005santy@gmail.com>
2024-03-06 11:34:01 +0100

    Created SavefileTest and added list, view and store tests

---
+++ tests/Feature/SavefileTest.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+use Illuminate\Http\Testing\File;
+use Illuminate\Testing\Fluent\AssertableJson;
+
+class SavefileTest extends TestCase
+{
+    /**
+     * A basic feature test example.
+     */
+    public function test_list_savefile(): void
+    {
+        $response = $this->get('/api/savefile');
+
+        $response->assertStatus(200);
+    }
+
+    public function test_view_savefile(): void
+    {
+        $response = $this->get('/api/savefile/1');
+
+        $response
+            ->assertStatus(200)
+            ->assertDownload();
+    }
+
+    public function test_store_savefile(): void
+    {
+        $faker = \Faker\Factory::create();
+
+        $file_name = $faker->word . $faker->fileExtension;
+        $response = $this->post('/api/savefile', [
+            'savefile' => File::fake()->create('savefile.txt'),
+            'file_name' => $file_name,
+            'fk_id_game' => '1'
+        ]);
+
+        $response
+            ->assertStatus(201)
+            ->assertJson(fn (AssertableJson $json) =>
+                $json->where('file_name', $file_name)
+                    ->where('fk_id_game', '1')
+                    ->etc()
+            );
+    }
+
+}
