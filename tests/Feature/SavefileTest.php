e7bb3b726583f0f6bb06b95c29c9281708a6574b
Giacomo Santilli <giaky2005santy@gmail.com>
2024-03-07 11:22:35 +0100

    Remove ExampleTests and rewritten SavefileTest

--- tests/Feature/SavefileTest.php
+++ tests/Feature/SavefileTest.php
@@ -2,17 +2,18 @@
 
 namespace Tests\Feature;
 
-use Illuminate\Foundation\Testing\RefreshDatabase;
-use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\DatabaseTransactions;
 use Tests\TestCase;
-use Illuminate\Http\Testing\File;
+use Illuminate\Http\UploadedFile;
 use Illuminate\Testing\Fluent\AssertableJson;
+use Faker\Factory;
+use Illuminate\Support\Facades\Storage;
 
 class SavefileTest extends TestCase
 {
-    /**
-     * A basic feature test example.
-     */
+
+    use DatabaseTransactions;
+
     public function test_list_savefile(): void
     {
         $response = $this->get('/api/savefile');
@@ -22,7 +23,7 @@ class SavefileTest extends TestCase
 
     public function test_get_savefile(): void
     {
-        $response = $this->get('/api/savefile/1');
+        $response = $this->get('/api/savefile/2');
 
         $response
             ->assertStatus(200)
@@ -31,22 +32,127 @@ class SavefileTest extends TestCase
 
     public function test_store_savefile(): void
     {
-        $faker = \Faker\Factory::create();
+        $faker = Factory::create();
 
-        $file_name = $faker->word . $faker->fileExtension;
+        // Create a file
+        $file = UploadedFile::fake()->create('savefile.txt', 256);
+        $file_name = $faker->word . '.' . $faker->fileExtension;
+        $fk_id_game = strval($faker->numberBetween(1, 10));
+
+        // Send the request
         $response = $this->post('/api/savefile', [
-            'savefile' => File::fake()->create('savefile.txt'),
+            'savefile' => $file,
             'file_name' => $file_name,
-            'fk_id_game' => '1'
+            'fk_id_game' => $fk_id_game
         ]);
 
+        // Check the response
         $response
             ->assertStatus(201)
             ->assertJson(fn (AssertableJson $json) =>
                 $json->where('file_name', $file_name)
-                    ->where('fk_id_game', '1')
+                    ->where('fk_id_game', $fk_id_game)
                     ->etc()
             );
+
+        // Check if the file was created
+        $this->assertFileEquals(
+            $file->getPathname(),
+            storage_path('app/saves/' . $file_name)
+        );
+
+        // Delete the file
+        Storage::delete('saves/' . $file_name);
+    }
+
+    public function test_store_savefile_no_file_name(): void
+    {
+        $faker = Factory::create();
+
+        // Create a file
+        $file_name = $faker->word . '.' . $faker->fileExtension;
+        $file = UploadedFile::fake()->create($file_name, 256);
+        $fk_id_game = strval($faker->numberBetween(1, 10));
+
+        // Send the request
+        $response = $this->post('/api/savefile', [
+            'savefile' => $file,
+            'fk_id_game' => $fk_id_game
+        ]);
+
+        // Check the response
+        $response
+            ->assertStatus(201)
+            ->assertJson(fn (AssertableJson $json) =>
+                $json->where('file_name', $file_name)
+                    ->where('fk_id_game', $fk_id_game)
+                    ->etc()
+            );
+
+        // Check if the file was created
+        $this->assertFileEquals(
+            $file->getPathname(),
+            storage_path('app/saves/' . $file_name)
+        );
+
+        // Delete the file
+        Storage::delete('saves/' . $file_name);
+    }
+
+    public function test_update_savefile(): void
+    {
+        // Create a file
+        $file = UploadedFile::fake()->create('savefile.txt', 256);
+        $file_name = \App\Models\Savefile::find(1)->file_name;
+        $fk_id_game = strval(Factory::create()->numberBetween(1, 10));
+
+        // Send the request
+        $response = $this->put('/api/savefile/1', [
+            'savefile' => $file,
+            'fk_id_game' => $fk_id_game
+        ]);
+
+        // Check the response
+        $response
+            ->assertStatus(200)
+            ->assertJson(fn (AssertableJson $json) =>
+                $json->where('fk_id_game', $fk_id_game)
+                    ->etc()
+            );
+
+        // Check if the file was updated
+        $this->assertFileEquals(
+            $file->getPathname(),
+            storage_path('app/saves/' . $file_name)
+        );
+
+        // Delete the file
+        $files = Storage::files('backups');
+        // Find all files that match the file name
+        $matchingFiles = array_filter($files, function ($file) use ($file_name) {
+            return strpos($file, $file_name) !== false;
+        });
+        // Delete the last created file
+        if (!empty($matchingFiles)) {
+            $lastCreatedFile = end($matchingFiles);
+            Storage::delete($lastCreatedFile);
+        }
+    }
+
+    public function test_delete_savefile(): void
+    {
+        // Create a backup of the file
+        $file_name = \App\Models\Savefile::find(1)->file_name;
+        Storage::copy('saves/' . $file_name, 'saves/' . $file_name . '.bak');
+
+        // Test the deletion
+        $response = $this->delete('/api/savefile/1');
+        $response->assertStatus(200);
+        $this->assertFileDoesNotExist('app/saves/' . $file_name);
+
+        // Restore the file
+        Storage::move('saves/' . $file_name . '.bak', 'saves/' . $file_name);
+
     }
 
 }
