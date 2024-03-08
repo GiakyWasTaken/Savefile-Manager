757f5833bce60c568351ec0ec53eb80c70024622
Giacomo Santilli <giaky2005santy@gmail.com>
2024-03-08 09:53:42 +0100

    Add UserAuthController and updated routes for user authentication

---
+++ app/Http/Controllers/UserAuthController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
+
+class UserAuthController extends Controller
+{
+    public function register (Request $request)
+    {
+        // Validate the request
+        $request->validate([
+            'name' => 'required|string',
+            'email' => 'required|email|unique:users',
+            'password' => 'required|confirmed'
+        ]);
+
+        // Create a new user
+        $user = User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => Hash::make($request->password)
+        ]);
+
+        // Return the user
+        return response($user, 201);
+    }
+
+    public function login (Request $request)
+    {
+        // Validate the request
+        $request->validate([
+            'email' => 'required|email',
+            'password' => 'required'
+        ]);
+
+        // Attempt to log the user in
+        if (auth()->attempt($request->only('email', 'password'))) {
+            // Return the user
+            $user = auth()->user();
+            $token = $user->createToken('user_token')->plainTextToken;
+            return response(['token' => $token], 200);
+        }
+
+        // Return an error
+        return response('Invalid login details', 401);
+    }
+
+    public function user (Request $request)
+    {
+        // Return the user
+        return response($request->user(), 200);
+    }
+
+    public function logout ()
+    {
+        // Log the user out
+        auth()->user()->tokens()->delete();
+
+        // Return a message
+        return response('Logged out', 200);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-use App\Models\Savefile;
+use App\Http\Controllers\UserAuthController;
 use App\Http\Controllers\SavefileController;
 use App\Http\Controllers\GameController;
 
@@ -17,12 +17,11 @@ use App\Http\Controllers\GameController;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
-
-Route::get('/database', function () {
-    return view('database');
+Route::post('/register', [UserAuthController::class, 'register']);
+Route::post('/login', [UserAuthController::class, 'login']);
+Route::group(['middleware' => ['auth:sanctum']], function () {
+    Route::get('/user', [UserAuthController::class, 'user']);
+    Route::get('/logout', [UserAuthController::class, 'logout']);
 });
 
 Route::get('savefile', [SavefileController::class, 'list']);
