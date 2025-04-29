f4cfaecafaff91a3c152d7200db03d4f9529fe1b
Giacomo Santilli <giaky2005santy@gmail.com>
2025-04-30 00:46:18 +0200

    Removed nginx form docker compose

--- docker-compose/nginx/ssl/generate-keys.sh
+++
@@ -1,4 +0,0 @@
-openssl genrsa -out "./default.key" 2048
-chmod 644 ./default.key
-openssl req -new -key "./default.key" -out "./default.csr" -subj "/CN=default/O=default/C=UK"
-openssl x509 -req -days 365 -in "./default.csr" -signkey "./default.key" -out "./default.crt"
